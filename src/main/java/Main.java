da6ac94ab469ef4bc326fd8449503adee8d97e0c
aviad otmazgin <otmazgin.aviad@gmail.com>
2014-04-17 12:49:47 +0300

    Matching those chick lips in her own photo using matchTemplate and minMaxLoc.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,8 @@
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
+import org.opencv.core.*;
 import utillities.Utilities;
 
-import static org.opencv.imgproc.Imgproc.COLOR_BGR2GRAY;
-import static org.opencv.imgproc.Imgproc.cvtColor;
+import static org.opencv.imgproc.Imgproc.TM_CCORR_NORMED;
+import static org.opencv.imgproc.Imgproc.matchTemplate;
 
 public class Main
 {
@@ -12,9 +11,19 @@ public class Main
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
         Mat image = Utilities.readImage("/1.jpg");
+        Mat template = Utilities.readImage("/lips.jpg");
 
-        cvtColor(image, image, COLOR_BGR2GRAY);
+        int result_cols = image.cols() - template.cols() + 1;
+        int result_rows = image.rows() - template.rows() + 1;
+        Mat result = new Mat(result_rows, result_cols, CvType.CV_32FC1);
 
-        Utilities.writeImageToFile(image, "grayImage.png");
+        matchTemplate(image, template, result, TM_CCORR_NORMED);
+
+        Core.MinMaxLocResult minMaxLocResult = Core.minMaxLoc(result);
+
+        Point maxLocation = minMaxLocResult.maxLoc;
+        Utilities.drawRect(new Rect(maxLocation, new Size(template.cols(), template.rows())), image);
+
+        Utilities.writeImageToFile(image, "match.jpg");
     }
 }
