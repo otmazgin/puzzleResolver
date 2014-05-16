f305a3e2b4e96ba74d4959f59f47043af687931b
aviad otmazgin <otmazgin.aviad@gmail.com>
2014-05-16 10:14:46 +0300

    minor changes

--- src/main/java/assembler/PuzzleAssembler.java
+++ src/main/java/assembler/PuzzleAssembler.java
@@ -9,6 +9,7 @@ import utillities.Utilities;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -19,7 +20,7 @@ public enum PuzzleAssembler
 
     private static final int numberOfCPUCores = 4;
 
-    public void assemblePieces(Collection<Mat> puzzlePieces, Mat puzzle, double[] backgroundColor)
+    public void assemblePieces(Collection<Mat> puzzlePieces, Mat puzzle, double[] backgroundColor) throws Exception
     {
         System.out.println("Started assembling at: " + new Date());
 
@@ -34,23 +35,19 @@ public enum PuzzleAssembler
 
         executorService.shutdown();
 
-        for (Future<Match> future : futures)
+        for (Future<Match> futureMatch : futures)
         {
-            try
-            {
-                Match match = future.get();
-                Rect matchRectangle = new Rect(match.getMatchPoint(), new Size(match.getWidth(), match.getHeight()));
-
-                Utilities.drawRect(matchRectangle, puzzle);
-            }
-            catch (Exception e)
-            {
-                e.printStackTrace();
-            }
+            Utilities.drawRect(getRectangleOf(futureMatch), puzzle);
         }
 
         Utilities.writeImageToFile(puzzle, "matches.jpg");
 
         System.out.println("Finished assembling at: " + new Date());
+    }
+
+    private Rect getRectangleOf(Future<Match> future) throws InterruptedException, ExecutionException
+    {
+        Match match = future.get();
+        return new Rect(match.getMatchPoint(), new Size(match.getWidth(), match.getHeight()));
     }
 }
--- src/main/java/assembler/PuzzlePieceRestorer.java
+++ src/main/java/assembler/PuzzlePieceRestorer.java
@@ -19,50 +19,42 @@ enum PuzzlePieceRestorer
     {
         Mat maskOfGaps = new Mat(puzzlePiece.size(), CvType.CV_8U);
 
-        inpaintLeftMiddleSuspectedGap(puzzlePiece, backgroundColor, maskOfGaps);
-        inpaintUpMiddleSuspectedGap(puzzlePiece, backgroundColor, maskOfGaps);
-        inpaintDownMiddleSuspectedGap(puzzlePiece, backgroundColor, maskOfGaps);
-        inpaintRightMiddleSuspectedGap(puzzlePiece, backgroundColor, maskOfGaps);
+        findLeftMiddleGap(puzzlePiece, backgroundColor, maskOfGaps);
+        findUpMiddleGap(puzzlePiece, backgroundColor, maskOfGaps);
+        findDownMiddleGap(puzzlePiece, backgroundColor, maskOfGaps);
+        findRightMiddleGap(puzzlePiece, backgroundColor, maskOfGaps);
 
         inpaint(puzzlePiece, maskOfGaps, puzzlePiece, 1, INPAINT_TELEA);
 
         //Utilities.writeImageToFile(puzzlePiece, "inpainted.jpg");
     }
 
-    private void inpaintLeftMiddleSuspectedGap(Mat puzzlePiece, double[] backgroundColor, Mat maskOfGaps)
+    private void findLeftMiddleGap(Mat puzzlePiece, double[] backgroundColor, Mat maskOfGaps)
     {
-        //Utilities.drawPoint(new org.opencv.core.Point(10, puzzlePiece.height() / 2), puzzlePiece);
-
         if (euclideanDistance(puzzlePiece.get(puzzlePiece.height() / 2, 10), backgroundColor) <= distanceFromBackgroundColorThreshold)
         {
             inpaintGapIfExists(puzzlePiece, maskOfGaps, new Point(0, puzzlePiece.height() / 4), backgroundColor);
         }
     }
 
-    private void inpaintUpMiddleSuspectedGap(Mat puzzlePiece, double[] backgroundColor, Mat maskOfGaps)
+    private void findUpMiddleGap(Mat puzzlePiece, double[] backgroundColor, Mat maskOfGaps)
     {
-        //Utilities.drawPoint(new org.opencv.core.Point(puzzlePiece.height() / 2, 10), puzzlePiece);
-
         if (euclideanDistance(puzzlePiece.get(10, puzzlePiece.width() / 2), backgroundColor) <= distanceFromBackgroundColorThreshold)
         {
             inpaintGapIfExists(puzzlePiece, maskOfGaps, new Point(puzzlePiece.width() / 4, 0), backgroundColor);
         }
     }
 
-    private void inpaintDownMiddleSuspectedGap(Mat puzzlePiece, double[] backgroundColor, Mat maskOfGaps)
+    private void findDownMiddleGap(Mat puzzlePiece, double[] backgroundColor, Mat maskOfGaps)
     {
-        //Utilities.drawPoint(new org.opencv.core.Point(puzzlePiece.width() / 2, puzzlePiece.height() - 10), puzzlePiece);
-
         if (euclideanDistance(puzzlePiece.get(puzzlePiece.height() - 10, puzzlePiece.width() / 2), backgroundColor) <= distanceFromBackgroundColorThreshold)
         {
             inpaintGapIfExists(puzzlePiece, maskOfGaps, new Point(puzzlePiece.width() / 4, puzzlePiece.height() / 2), backgroundColor);
         }
     }
 
-    private void inpaintRightMiddleSuspectedGap(Mat puzzlePiece, double[] backgroundColor, Mat maskOfGaps)
+    private void findRightMiddleGap(Mat puzzlePiece, double[] backgroundColor, Mat maskOfGaps)
     {
-        //Utilities.drawPoint(new org.opencv.core.Point(puzzlePiece.width() - 10, puzzlePiece.height() / 2), puzzlePiece);
-
         if (euclideanDistance(puzzlePiece.get(puzzlePiece.height() / 2, puzzlePiece.width() - 10), backgroundColor) <= distanceFromBackgroundColorThreshold)
         {
             inpaintGapIfExists(puzzlePiece, maskOfGaps, new Point(puzzlePiece.width() / 2, puzzlePiece.height() / 4), backgroundColor);
@@ -71,8 +63,6 @@ enum PuzzlePieceRestorer
 
     private void inpaintGapIfExists(Mat puzzlePiece, Mat maskOfGaps, Point suspectedGapStart, double[] backgroundColor)
     {
-        //Utilities.drawRect(new Rect(suspectedGapStart.x, suspectedGapStart.y, puzzlePiece.width() / 2, puzzlePiece.height() / 2), puzzlePiece);
-
         for (int row = suspectedGapStart.y; row < suspectedGapStart.y + puzzlePiece.height() / 2; row++)
         {
             for (int column = suspectedGapStart.x; column < suspectedGapStart.x + puzzlePiece.width() / 2; column++)
@@ -124,8 +114,5 @@ enum PuzzlePieceRestorer
         }
 
         inpaint(puzzlePiece, maskOfGaps, puzzlePiece, 1, INPAINT_TELEA);
-
-        Utilities.writeImageToFile(puzzlePiece, "inpainted.jpg");
-
     }
 }
