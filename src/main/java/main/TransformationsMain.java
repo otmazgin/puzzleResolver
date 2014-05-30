d9b067b37b82744dae4bd7912def531ef53791da
avihad <avihad87@gmail.com>
2014-05-30 16:40:12 +0300

    add rotation to the complete piece for the contour drawing

--- src/main/java/main/TransformationsMain.java
+++ src/main/java/main/TransformationsMain.java
@@ -8,80 +8,100 @@ import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
 
-import entities.PuzzlePiece;
 import pieceRecognizer.PuzzlePieceCornerDetector;
 import pieceRecognizer.PuzzlePieceDetector;
 import transformations.AffineTransformator;
 import transformations.PuzzlePieceTransformer;
 import utillities.Utilities;
 import entities.Puzzle;
+import entities.PuzzlePiece;
 
-public class TransformationsMain
-{
-    public static void main(String[] args) throws Exception
-    {
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+public class TransformationsMain {
+    public static void main(String[] args) throws Exception {
+	System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
-        Mat piecesImage = Utilities.readImage("test20.jpg");
-        Mat puzzleImg = Utilities.readImage("IMG_1062.jpg");
-        puzzleImg = new Mat(puzzleImg, new Rect(new Point(249, 2277), new Point(3062, 308)));
-        int harisBlockSize = 2;
-        int harisApertureSize = 5;
-        double harisK = 0.05;
-        int harisThreashHold = 0;
-
-        List<PuzzlePiece> pieces = PuzzlePieceDetector.pieceDetector(piecesImage, 0, 0, 0, 0, 0, 0,
-                Boolean.TRUE);
-
-        int count = 1;
-        for (PuzzlePiece puzzlePiece : pieces)
-        {
-            PuzzlePieceCornerDetector.cornerFindeByQuaters(harisBlockSize, harisApertureSize, harisK,
-                    harisThreashHold, puzzlePiece);
-            Utilities.writeImageToFile(puzzlePiece.getPiece(), "BP" + count + ".jpg");
-            count++;
-        }
-
-        Puzzle puzzle = PuzzlePieceTransformer.instance.transformPieces(puzzleImg, pieces, 5, 4);
-
-        Utilities.writeImageToFile(puzzle.getCompletePuzzle(), "p0.jpg");
-        int i = 1;
-        for (PuzzlePiece piece : puzzle.getPieces())
-        {
-            Utilities.writeImageToFile(piece.getTransformedPieceMatrix(), "P" + i + ".jpg");
-            i++;
-        }
+	testAffineConturTransf();
+	// testAffineWithCornerDetection();
     }
 
-    private static void testAffineTransform() throws Exception
-    {
-        String turtlePiecePath = ClassLoader.getSystemResource("turtle_one_piece.png").getFile();
-        String turtlePuzzlePath = ClassLoader.getSystemResource("turtle_puzzle.png").getFile();
+    private static void testAffineWithCornerDetection() throws Exception {
+	Mat piecesImage = Utilities.readImage("test20.jpg");
+	Mat puzzleImg = Utilities.readImage("IMG_1062.jpg");
+	puzzleImg = new Mat(puzzleImg, new Rect(new Point(249, 2277), new Point(3062, 308)));
+	int harisBlockSize = 2;
+	int harisApertureSize = 5;
+	double harisK = 0.05;
+	int harisThreashHold = 0;
 
-        Mat puzzlePiece = Utilities.readImage(turtlePiecePath);
-        Mat puzzle = Utilities.readImage(turtlePuzzlePath);
+	List<PuzzlePiece> pieces = PuzzlePieceDetector.pieceDetector(piecesImage, 0, 0, 0, 0, 0, 0,
+		Boolean.TRUE);
 
-        int puzzleWidth = 400;
-        int puzzleHeight = 300;
-        Mat dstPuzzleImage = new Mat(new Size(puzzleWidth, puzzleHeight), puzzle.type());
-        List<Point> srcPuzzlePoints = Arrays.asList(new Point(0, 0), new Point(0, puzzle.height()),
-                new Point(puzzle.width(), puzzle.height()));
-        List<Point> dstPuzzlePoints = Arrays.asList(new Point(0, 0), new Point(0, dstPuzzleImage.height()),
-                new Point(dstPuzzleImage.width(), dstPuzzleImage.height()));
-        Mat transformedPuzzle = AffineTransformator.instance.transform(puzzle, dstPuzzleImage,
-                srcPuzzlePoints, dstPuzzlePoints);
+	int count = 1;
+	for (PuzzlePiece puzzlePiece : pieces) {
+	    PuzzlePieceCornerDetector.cornerFindeByQuaters(harisBlockSize, harisApertureSize, harisK,
+		    harisThreashHold, puzzlePiece);
+	    Utilities.writeImageToFile(puzzlePiece.getPiece(), "BP" + count + ".jpg");
+	    count++;
+	}
 
-        Mat dstPieceImage = new Mat(new Size(puzzleHeight / 2, puzzleWidth / 3), puzzlePiece.type());
-        List<Point> srcPiecePoints = Arrays.asList(new Point(7, 9), new Point(128, 9), new Point(128, 136));
+	Puzzle puzzle = PuzzlePieceTransformer.instance.transformPieces(puzzleImg, pieces, 5, 4);
 
-        List<Point> dstPiecePoints = Arrays.asList(new Point(0, 0), new Point(dstPieceImage.width(), 0),
-                new Point(dstPieceImage.width(), dstPieceImage.height()));
-        ;
-        Mat transformedPiece = AffineTransformator.instance.transform(puzzlePiece, dstPieceImage,
-                srcPiecePoints, dstPiecePoints);
+	Utilities.writeImageToFile(puzzle.getCompletePuzzle(), "p0.jpg");
+	int i = 1;
+	for (PuzzlePiece piece : puzzle.getPieces()) {
+	    Utilities.writeImageToFile(piece.getTransformedPieceMatrix(), "P" + i + ".jpg");
+	    i++;
+	}
+    }
 
-        Utilities.writeImageToFile(dstPuzzleImage, "puzz_trans.png");
-        Utilities.writeImageToFile(dstPieceImage, "piece_rans.png");
+    private static void testAffineTransform() throws Exception {
+	String turtlePiecePath = ClassLoader.getSystemResource("turtle_one_piece.png").getFile();
+	String turtlePuzzlePath = ClassLoader.getSystemResource("turtle_puzzle.png").getFile();
+
+	Mat puzzlePiece = Utilities.readImage(turtlePiecePath);
+	Mat puzzle = Utilities.readImage(turtlePuzzlePath);
+
+	int puzzleWidth = 400;
+	int puzzleHeight = 300;
+	Mat dstPuzzleImage = new Mat(new Size(puzzleWidth, puzzleHeight), puzzle.type());
+	List<Point> srcPuzzlePoints = Arrays.asList(new Point(0, 0), new Point(0, puzzle.height()),
+		new Point(puzzle.width(), puzzle.height()));
+	List<Point> dstPuzzlePoints = Arrays.asList(new Point(0, 0), new Point(0, dstPuzzleImage.height()),
+		new Point(dstPuzzleImage.width(), dstPuzzleImage.height()));
+	Mat transformedPuzzle = AffineTransformator.instance.transform(puzzle, dstPuzzleImage,
+		srcPuzzlePoints, dstPuzzlePoints);
+
+	Mat dstPieceImage = new Mat(new Size(puzzleHeight / 2, puzzleWidth / 3), puzzlePiece.type());
+	List<Point> srcPiecePoints = Arrays.asList(new Point(7, 9), new Point(128, 9), new Point(128, 136));
+
+	List<Point> dstPiecePoints = Arrays.asList(new Point(0, 0), new Point(dstPieceImage.width(), 0),
+		new Point(dstPieceImage.width(), dstPieceImage.height()));
+	;
+	Mat transformedPiece = AffineTransformator.instance.transform(puzzlePiece, dstPieceImage,
+		srcPiecePoints, dstPiecePoints);
+
+	Utilities.writeImageToFile(dstPuzzleImage, "puzz_trans.png");
+	Utilities.writeImageToFile(dstPieceImage, "piece_rans.png");
+    }
+
+    public static void testAffineConturTransf() throws Exception {
+
+	Mat puzzlePart = Utilities.readImage("BP7.jpg");
+	Mat extendedPart = new Mat(new Size(puzzlePart.size().width * 1.5, puzzlePart.size().height * 1.5),
+		puzzlePart.type());
+	Imgproc.copyMakeBorder(puzzlePart, extendedPart, 0, 0, 0, 0, Imgproc.BORDER_DEFAULT);
+	Point center = new Point(puzzlePart.size().width / 2, puzzlePart.size().height / 2);
+
+	Point p1 = new Point(524, 768);
+	double angle = Math.toDegrees(Math.atan((768.0 - 347.0) / (523.0 - 754.0)));
+	Point p2 = new Point(754, 347);
+
+	Mat rotateMatrix = Imgproc.getRotationMatrix2D(center, angle, 0.75);
+	Imgproc.warpAffine(puzzlePart, extendedPart, rotateMatrix, puzzlePart.size());
+
+	Utilities.writeImageToFile(extendedPart, "extendedPart.jpg");
+
     }
 }
--- src/main/java/transformations/PuzzlePieceTransformer.java
+++ src/main/java/transformations/PuzzlePieceTransformer.java
@@ -8,67 +8,96 @@ import java.util.List;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
+import org.opencv.core.Size;
 
 import pieceRecognizer.Corner;
-import entities.PuzzlePiece;
 import entities.Puzzle;
+import entities.PuzzlePiece;
 
-public enum PuzzlePieceTransformer
-{
+public enum PuzzlePieceTransformer {
     instance;
 
-    private static final int WIDTH = 1600;
+    private static final int WIDTH  = 1600;
     private static final int HEIGHT = 1200;
 
-    public Puzzle transformPieces(Mat puzzleImg, List<PuzzlePiece> pieces, int puzzleColumns, int puzzleRows)
-    {
-        List<PuzzlePiece> transformedPieces = new ArrayList<>();
-        List<Point> completedPuzzlePoints = Arrays.asList(new Point(0, 0), new Point(0, puzzleImg.height()),
-                new Point(puzzleImg.width(), puzzleImg.height()));
-        Mat transformedPuzzle = AffineTransformator.instance.transform(puzzleImg, completedPuzzlePoints,
-                WIDTH, HEIGHT);
+    public Puzzle transformPieces(Mat puzzleImg, List<PuzzlePiece> pieces, int puzzleColumns, int puzzleRows) {
+	List<PuzzlePiece> transformedPieces = new ArrayList<>();
+	List<Point> completedPuzzlePoints = Arrays.asList(new Point(0, 0), new Point(0, puzzleImg.height()),
+		new Point(puzzleImg.width(), puzzleImg.height()));
+	Mat transformedPuzzle = AffineTransformator.instance.transform(puzzleImg, completedPuzzlePoints,
+		WIDTH, HEIGHT);
 
-        for (PuzzlePiece puzzlePiece : pieces)
-        {
+	for (PuzzlePiece puzzlePiece : pieces) {
 
-            List<Point> pieceTransformPoints = adjustAndOrderPiecePointsForTransf(
-                    puzzlePiece.getCornersList(), puzzlePiece.getRect(), puzzleColumns > puzzleRows);
+	    boolean isPieceInLandscape = puzzleColumns > puzzleRows;
+	    List<Point> pieceTransformPoints = adjustAndOrderPiecePointsForTransf(
+		    puzzlePiece.getCornersList(), puzzlePiece.getRect(), isPieceInLandscape);
 
-            if (pieceTransformPoints.isEmpty())
-            {
-                continue;
-            }
+	    if (pieceTransformPoints.isEmpty()) {
+		continue;
+	    }
 
-            Mat transformedPiece = AffineTransformator.instance.transform(puzzlePiece.getPiece(),
-                    pieceTransformPoints, WIDTH / puzzleColumns, HEIGHT / puzzleRows);
+	    Mat puzzlePieceMat = puzzlePiece.getPiece();
+	    int transPuzlePieceWidth = WIDTH / puzzleColumns;
+	    int transPuzzlePieceHeight = HEIGHT / puzzleRows;
 
-            puzzlePiece.setTransformedPieceMatrix(transformedPiece);
+	    // Transform the piece for the template matching
+	    Mat transformedPiece = AffineTransformator.instance.transform(puzzlePieceMat,
+		    pieceTransformPoints, transPuzlePieceWidth, transPuzzlePieceHeight);
 
-            transformedPieces.add(puzzlePiece);
-        }
+	    puzzlePiece.setTransformedPieceMatrix(transformedPiece);
 
-        return new Puzzle(transformedPuzzle, transformedPieces);
+	    transformedPieces.add(puzzlePiece);
+
+	    // Transform the original piece to strait the contour
+	    double growFactor = 2;
+	    Mat rotatedImage = new Mat(new Size(transPuzlePieceWidth * (1 + growFactor),
+		    transPuzzlePieceHeight * (1 + growFactor)), puzzlePieceMat.type());
+
+	    Point rotatedImageMiddlePoint = new Point(rotatedImage.width() / 2, rotatedImage.height() / 2);
+	    List<Point> rotatedPiecePoints = Arrays.asList(new Point(rotatedImageMiddlePoint.x
+		    - transPuzlePieceWidth / 2, rotatedImageMiddlePoint.y - transPuzzlePieceHeight / 2),
+		    new Point(rotatedImageMiddlePoint.x - transPuzlePieceWidth / 2, rotatedImageMiddlePoint.y
+			    + transPuzzlePieceHeight / 2), new Point(rotatedImageMiddlePoint.x
+			    + transPuzlePieceWidth / 2, rotatedImageMiddlePoint.y + transPuzzlePieceHeight
+			    / 2));
+
+	    AffineTransformator.instance.transform(puzzlePieceMat, rotatedImage, pieceTransformPoints,
+		    rotatedPiecePoints);
+
+	    puzzlePiece.setRotatedImage(rotatedImage);
+	}
+
+	return new Puzzle(transformedPuzzle, transformedPieces);
 
     }
 
     private List<Point> adjustAndOrderPiecePointsForTransf(List<Corner> cornersList, Rect boundingBox,
-                                                           boolean isLandscape)
-    {
-        if (cornersList.size() != 4)
-        {
-            return Collections.emptyList();
-        }
+	    boolean isLandscape) {
+	if (cornersList.size() != 4) {
+	    return Collections.emptyList();
+	}
 
-        for (Corner corner : cornersList)
-        {
-            corner.setX(corner.getX() - boundingBox.x);
-            corner.setY(corner.getY() - boundingBox.y);
-        }
-        double firstDiff = cornersList.get(0).distance(cornersList.get(1));
-        double secondDiff = cornersList.get(1).distance(cornersList.get(2));
-        boolean isPieceInLandscape = secondDiff - firstDiff > 0;
+	for (Corner corner : cornersList) {
+	    corner.setX(corner.getX() - boundingBox.x);
+	    corner.setY(corner.getY() - boundingBox.y);
+	}
 
-        return isPieceInLandscape && isLandscape ? Corner.toPoints(cornersList.subList(0,
-                cornersList.size() - 1)) : Corner.toPoints(cornersList.subList(1, cornersList.size()));
+	boolean isPieceInLandscape = isPiecePointsInLandscape(cornersList);
+
+	return isPieceInLandscape && isLandscape ? Corner.toPoints(cornersList.subList(0,
+		cornersList.size() - 1)) : Corner.toPoints(cornersList.subList(1, cornersList.size()));
+    }
+
+    private boolean isPiecePointsInLandscape(List<Corner> cornersList) {
+
+	boolean isPieceInLandscape = Boolean.FALSE;
+	if (cornersList.size() >= 3) {
+	    double firstDiff = cornersList.get(0).distance(cornersList.get(1));
+	    double secondDiff = cornersList.get(1).distance(cornersList.get(2));
+	    isPieceInLandscape = secondDiff - firstDiff > 0;
+	}
+
+	return isPieceInLandscape;
     }
 }
